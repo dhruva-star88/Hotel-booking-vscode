50d224bfbd655a82f8f5334c75afbd9e1c79b8b6
dhruva-star88 <dhruvatej6565@gmail.com>
2024-03-24 20:07:17 +0530

    Added Card Security

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 df_cards = pd.read_csv("cards.csv", dtype=str)
-print(df_cards)
-enquire = df_cards.loc[df_cards["number"] == "1234", "number"].squeeze()
-print(enquire)
+df_sec_card = pd.read_csv("card_security.csv", dtype=str)
+print(df_sec_card)
+password = df_sec_card.loc[df_sec_card["number"] == "1234", "password"].squeeze()
+print(password)
